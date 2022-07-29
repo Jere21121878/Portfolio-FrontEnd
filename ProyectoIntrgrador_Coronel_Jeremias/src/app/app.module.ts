5707e0808461f2071bc10590eab517ade647c149
Jere21121878 <ds21tn.coronel.jeremias@gmail.com>
2022-07-29 14:19:16 -0300

    header primera parte

--- ProyectoIntrgrador_Coronel_Jeremias/src/app/app.module.ts
+++ ProyectoIntrgrador_Coronel_Jeremias/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componente/header/header.component';
+import { LogoArproComponent } from './componente/logo-arpro/logo-arpro.component';
+import { RedesComponent } from './componente/redes/redes.component';
+import { BannerComponent } from './componente/banner/banner.component';
+import { AcercaDeComponent } from './componente/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArproComponent,
+    RedesComponent,
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
